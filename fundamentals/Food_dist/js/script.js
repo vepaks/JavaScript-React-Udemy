bdb7a5cbd1ac454260f0a05480dcabc21c599326
vepaks <kamburoff@gmail.com>
2023-02-02 10:36:49 +0200

    02.02.2023г.

---
+++ fundamentals/Food_dist/js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+            tabsContent = document.querySelectorAll('.tabcontent'),
+            tabsParent = document.querySelector('.tabheader__items');
+
+});
--- fundamentals/project_mDB/project/js/script.js
+++ fundamentals/project_mDB/project/js/script.js
@@ -17,63 +17,86 @@ P.S. Здесь есть несколько вариантов решения з
 
 "use strict";
 
-const adv = document.querySelectorAll(".promo__adv img");
-const genre = document.querySelector(".promo__genre");
-const bg = document.querySelector(".promo__bg");
-const movieList = document.querySelector(".promo__interactive-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const adv = document.querySelectorAll(".promo__adv img");
+  const genre = document.querySelector(".promo__genre");
+  const bg = document.querySelector(".promo__bg");
+  const movieList = document.querySelector(".promo__interactive-list");
+  const addForm = document.querySelector("form.add");
+  const addInput = addForm.querySelector(".adding__input");
+  const checkbox = addForm.querySelector('[type="checkbox"]');
 
-const movieDB = {
-  movies: [
-    "Логан",
-    "Лига справедливости",
-    "Ла-ла лэнд",
-    "Одержимость",
-    "Скотт Пилигрим против...",
-  ],
-};
+  const movieDB = {
+    movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против...",
+    ],
+  };
+  //функция за изтриване на реклами
+  function deleteAdv(arr) {
+    arr.forEach((item) => {
+      item.remove();
+    });
+  }
+  //функция за сортиране на масив
+  function sortArr(arr) {
+    arr.sort();
+  }
+  const makeChanges = () => {
+    genre.textContent = "драма";
+    bg.style.backgroundImage = 'url("img/bg.jpg")';
+  };
 
-adv.forEach((item) => {
-  item.remove();
-});
+  //функция за създаване на списък с филми
+  function createMovieList(films, parent) {
+    parent.innerHTML = "";
+    sortArr(movieDB.movies);
 
-genre.textContent = "драма";
+    films.forEach((film, i) => {
+      parent.innerHTML += `
+          <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+            </li>`;
+    });
 
-bg.style.backgroundImage = 'url("img/bg.jpg")';
+    const deleteBtn = document.querySelectorAll(".delete");
+    deleteBtn.forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        btn.parentElement.remove();
+        movieDB.movies.splice(i, 1);
 
-movieList.innerHTML = "";
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-  movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-        `;
-});
-
-const addForm = document.querySelector("form.add"),
-    addInput = addForm.querySelector(".adding__input"),
-    checkbox = addForm.querySelector('[type="checkbox"]');
-
-addForm.addEventListener("submit", (event) => {
+        createMovieList(films, parent);
+      });
+    });
+  }
+  //добавяме на евент при натискане на бутон
+  addForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
     let newFilm = addInput.value;
     const favorite = checkbox.checked;
 
     if (newFilm) {
-        if (newFilm.length > 21) {
-            newFilm = `${newFilm.substring(0, 22)}...`;
-        }
+      if (newFilm.length > 21) {
+        newFilm = `${newFilm.substring(0, 22)}...`;
+      }
 
-        if (favorite) {
-            console.log("Добавляем любимый фильм");
-        }
+      if (favorite) {
+        console.log("Добавляем любимый фильм");
+      }
 
-        movieDB.movies.push(newFilm);
-        sortArr(movieDB.movies);
+      movieDB.movies.push(newFilm);
 
-        createMovieList(movieDB.movies, movieList);
+      sortArr(movieDB.movies);
+      createMovieList(movieDB.movies, movieList);
     }
-
     event.target.reset();
+  });
+
+  createMovieList(movieDB.movies, movieList);
+  deleteAdv(adv);
+  makeChanges();
 });
